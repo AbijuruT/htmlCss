30172009ce2e9bfc30ed4436e929fecb14cc732a
AbijuruT <abijuruyves@gmail.com>
2022-04-04 15:20:01 +0200

    Added toast

--- Toast/js/scripts.js
+++ Toast/js/scripts.js
@@ -1,22 +1,27 @@
 const notificationBtn = document.getElementById("show-not");
 const container = document.getElementById("container");
 
-notificationBtn.addEventListener("click", () => {
-  showToast();
-});
+// Click event
+notificationBtn.addEventListener("click", showToast);
 
 function showToast() {
-  const words = [
+  const sentences = [
     "All human knowledge belongs to Earth!",
     "Loving someone means loving yourself!",
     "True love exist but we don't see it coz now days everything is iffy",
-  ]; 
-    
+    "The only way to great the work is loving what you do.",
+    "Truth and love always win, why can't we stay with'em?",
+  ];
+
   const toast = document.createElement("div");
   toast.classList.add("toast__notification");
-  const randomWord = Math.floor(Math.random() * words.length);
-  toast.textContent = words[randomWord];
+  // Randomly pick a sentence from array
+  const randomWord = Math.floor(Math.random() * sentences.length);
+  // Show text on screen
+  toast.textContent = sentences[randomWord];
   container.appendChild(toast);
+
+  // removing toast element from the dom after 3 seconds
   setTimeout(() => {
     toast.remove();
   }, 3000);
